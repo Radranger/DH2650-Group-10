d744167eef7e5eb75815581ed82d7f80b80aeddc
Patrik Lundqvist <ph.lundqvist@gmail.com>
2024-04-10 19:51:17 +0200

    feat(script): Add scripts for camera controls and trap activation

--- Parcel Pandemonium/Assets/Scripts/DMInteractions.cs
+++ Parcel Pandemonium/Assets/Scripts/DMInteractions.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class DMInteractions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject[] gameObjects;
+    
     void Update()
     {
-        
+
+        // // runs the gameObject functions when pressing numpad keys
+        // if (Input.GetKeyDown(KeyCode.Keypad1))
+        // {
+        //     gameObjects[0].GetComponent<DMObject>().Interact();
+        // }
+        // if (Input.GetKeyDown(KeyCode.Keypad2))
+        // {
+        //     gameObjects[1].GetComponent<DMObject>().Interact();
+        // }
+        // if (Input.GetKeyDown(KeyCode.Keypad3))
+        // {
+        //     gameObjects[2].GetComponent<DMObject>().Interact();
+        // }
+        // // runs the gameObjects functions when pressing gamepad buttons
+        // if (Input.GetKeyDown(KeyCode.JoystickButton0))
+        // {
+        //     gameObjects[0].GetComponent<DMObject>().Interact();
+        // }
+        // if (Input.GetKeyDown(KeyCode.JoystickButton1))
+        // {
+        //     gameObjects[1].GetComponent<DMObject>().Interact();
+        // }
+        // if (Input.GetKeyDown(KeyCode.JoystickButton2))
+        // {
+        //     gameObjects[2].GetComponent<DMObject>().Interact();
+        // }
+
     }
 }
