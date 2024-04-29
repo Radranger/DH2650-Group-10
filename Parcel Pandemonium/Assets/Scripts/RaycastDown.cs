5945513a0d3a6e1813777f8b03ac78d189ded268
Patrik Lundqvist <ph.lundqvist@gmail.com>
2024-04-29 11:44:59 +0200

    fix: fall trap bug

--- Parcel Pandemonium/Assets/Scripts/RaycastDown.cs
+++ Parcel Pandemonium/Assets/Scripts/RaycastDown.cs
@@ -6,6 +6,7 @@ public class RaycastDown : MonoBehaviour
 {
     public GameObject followObject; // Object to follow
     public float offset = 2.0f; // Speed of movement
+    private float latestYValue;
     // place object on the ground
     void Update()
     {
@@ -13,7 +14,12 @@ public class RaycastDown : MonoBehaviour
         // Raycast to the ground but still follow the object
         if (Physics.Raycast(transform.position, Vector3.down, out hit, 100))
         {
-            transform.position = new Vector3(followObject.transform.position.x, hit.point.y+offset, followObject.transform.position.z);
+            latestYValue = hit.point.y;
+            transform.position = new Vector3(followObject.transform.position.x, latestYValue+offset, followObject.transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(followObject.transform.position.x, latestYValue, followObject.transform.position.z);
         }
 
     }
--- Parcel Pandemonium/Assets/Scripts/ThrowObject.cs
+++ Parcel Pandemonium/Assets/Scripts/ThrowObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ThrowObject : MonoBehaviour
 {
-    public float throwForce = 40f;
+    public float throwForce = 10f;
     private GameObject throwableObject;
     public float respawnTime = 5.0f;
     private bool isThrown = false;
@@ -13,21 +13,25 @@ public class ThrowObject : MonoBehaviour
     {
         throwableObject = transform.GetChild(0).gameObject;
     }
+    void Update()
+    {
+        // Keeps the gameObject from rotating
+        throwableObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
 
     public void Throw()
     {
+        // Stops the player from throwing the object multiple times
         if (isThrown)
         {
             return;
         }
+
         isThrown = true;
         Debug.Log("Throwing object");
         throwableObject.GetComponent<Rigidbody>().isKinematic = false;
-        throwableObject.transform.GetChild(0).gameObject.GetComponent<Rigidbody>().isKinematic = false;
-         throwableObject.GetComponent<Rigidbody>().AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
-        // Add some rotation to the object
-         throwableObject.transform.GetChild(0).gameObject.GetComponent<Rigidbody>().AddTorque(new Vector3(0, 0, 10), ForceMode.VelocityChange);
-        
+        throwableObject.GetComponent<Rigidbody>().AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+        throwableObject.transform.GetChild(0).GetComponent<Rigidbody>().AddTorque(transform.right * throwForce, ForceMode.VelocityChange);
         StartCoroutine(RespawnCoroutine(respawnTime));
     }
 
@@ -35,9 +39,10 @@ public class ThrowObject : MonoBehaviour
     {
         yield return new WaitForSeconds(respawnTime);
         throwableObject.transform.position = transform.position;
-        throwableObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         throwableObject.GetComponent<Rigidbody>().isKinematic = true;
-        throwableObject.transform.GetChild(0).gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        throwableObject.transform.GetChild(0).GetComponent<Rigidbody>().rotation = Quaternion.Euler(0, 0, 0);
+        throwableObject.transform.GetChild(0).GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        throwableObject.transform.GetChild(1).transform.localPosition = new Vector3(0, -2, 0);
 
         isThrown = false;
         
