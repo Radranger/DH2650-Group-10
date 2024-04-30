9227e0ead254ab9583f57af4c929369eb1f78f7d
NineTong55 <zhaorui1203@gmail.com>
2024-04-30 18:37:52 +0200

    refactor(driver control): player movement
    -refined movement
    -added acceleration
    -added reversed functionality
    -added preliminary tilt functionality (does not work)
    
    Co-authored-by: William R <rolfman04@yahoo.se>
    Co-Authored-By: Radranger <34709766+Radranger@users.noreply.github.com>

--- Parcel Pandemonium/Assets/Scripts/PlayerMovement.cs
+++ Parcel Pandemonium/Assets/Scripts/PlayerMovement.cs
@@ -4,18 +4,76 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotationSpeed = 10f;
+    public float speed;
+    public float rotationSpeed;
+    public float maxSpeed;
+    private float currentSpeed = 0.0f;
+    public float tiltAngle;
+    private float reverseSpeed = 0.0f;
+
 
     void Update()
     {
+        // Get input from the "W" key
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Increase currentSpeed gradually up to the maxSpeed
+            currentSpeed = Mathf.Min(currentSpeed + (maxSpeed * Time.deltaTime), maxSpeed);
+        }
+        else
+        {
+            // If "W" key is not pressed, gradually decrease currentSpeed back to 0
+            currentSpeed = Mathf.Max(currentSpeed - (maxSpeed * Time.deltaTime), 0.0f);
+        }
+
+        // Get input from the "W" key
+        if (Input.GetKey(KeyCode.S) && currentSpeed == 0)
+        {
+            reverseSpeed = 2.0f;
+        }
+        else
+        {
+            reverseSpeed = 0.0f;
+        }
+
+        //tilt angle
+        float tiltAmount = 0.0f;
+        if (Input.GetKey(KeyCode.A))
+        {
+            tiltAmount = tiltAngle;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            tiltAmount = -tiltAngle;
+        }
+
+        //if speed = 0 disable rotation
+        if (currentSpeed == 0.0f && reverseSpeed == 0.0f)
+        {
+            rotationSpeed = 0.0f;
+        }
+        else
+        {
+            rotationSpeed = 2.5f;
+        }
+
+        Quaternion targetTilt = Quaternion.Euler(0, 0, tiltAmount);
+        //transform.rotation = Quaternion.Lerp(transform.rotation, targetTilt, Time.deltaTime * 5.0f);
+
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+
+        float rotationInput = horizontalInput * speed * Time.deltaTime * rotationSpeed;
+        transform.Rotate(0f, rotationInput, 0f);
+        transform.Rotate(Vector3.up * horizontalInput * rotationSpeed);
+
+
+        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+        transform.Translate(Vector3.back * reverseSpeed * Time.deltaTime);
+
+        /*float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
-        transform.Translate(movement);
-
-        float rotationInput = horizontalInput * speed * Time.deltaTime* rotationSpeed;
-        transform.Rotate(0f, rotationInput, 0f);
+        transform.Translate(movement);*/
     }
 }
+    
