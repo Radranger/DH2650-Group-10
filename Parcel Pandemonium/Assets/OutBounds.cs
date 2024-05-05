a5ed2c7d4a661be847d0ed6427a3b3bc6da2e826
William Zhan <zhaorui1203@gmail.com>
2024-05-06 01:19:07 +0200

    fix(Deathzone): respawn position and rotation
    - Fixed correct respawn position and rotation of the player and the camera.
    -Fixed where the player won't respawn when colliding with the death zone
    
    Co-Authored-By: Radranger <34709766+Radranger@users.noreply.github.com>

--- Parcel Pandemonium/Assets/OutBounds.cs
+++ Parcel Pandemonium/Assets/OutBounds.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class OutBounds : MonoBehaviour
 {
+        public Transform player;
         public Transform respawnPoint;
 
-        private void OnTriggerEnter(Collider other)
+        private float cameraDistanceX = 0f; 
+        private float cameraDistanceY = 1.48f; 
+        private float cameraDistanceZ = -3.61f; 
+
+
+    private Rigidbody playerRigidbody; 
+
+    private void Start()
+    {
+        playerRigidbody = player.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
-        Debug.Log("Trigger entered by: " + other.name);
-            if (other.CompareTag("Player"))
+            player.transform.position = respawnPoint.transform.position;
+            player.transform.rotation = respawnPoint.transform.rotation;
+
+            Transform playerTransform = player.Find("Vehicle01_Vespa"); 
+            if (playerTransform != null)
             {
-                Debug.Log("Player died!");
-                RespawnPlayer(other.gameObject);
+                playerTransform.localPosition = Vector3.zero;
+                playerTransform.localRotation = Quaternion.Euler(270f, 0f, 0f); 
+            }
+        
+            Transform cameraTransform = player.Find("Camera (1)"); 
+            if (cameraTransform != null)
+            {
+                cameraTransform.localPosition = new Vector3(cameraDistanceX, cameraDistanceY, cameraDistanceZ);
+                cameraTransform.localRotation = Quaternion.Euler(new Vector3(8f, 0f, 0f));
+            }
+
+            if (playerRigidbody != null)
+            {
+                playerRigidbody.velocity = Vector3.zero;
+                playerRigidbody.angularVelocity = Vector3.zero;
             }
         }
+    }
 
-        private void RespawnPlayer(GameObject Player)
-        {
-            Player.transform.position = respawnPoint.position;
-            Player.transform.rotation = respawnPoint.rotation;
-        }
 }
