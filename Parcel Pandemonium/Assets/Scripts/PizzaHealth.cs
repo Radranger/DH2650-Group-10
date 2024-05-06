4fcb588c3738e714d69b9ca72a5e38d2b5c715b9
Konstantina Maria Sourmpati <kmsou@kth.se>
2024-05-06 10:13:52 +0200

    feat (Player UI ): pizzas health element

---
+++ Parcel Pandemonium/Assets/Scripts/PizzaHealth.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PizzaHealth : MonoBehaviour
+{
+    public int pizzas;
+    public int maxpizzas;
+
+    public Sprite emptyPizza;
+    public Sprite fullPizza;
+    public Image[] pizzaStack;
+    private bool hasCollided = false; // Flag to track if collision has occurred
+
+    public PlayerMovement playerMovement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < pizzaStack.Length; i++)
+        {
+
+            if (i < pizzas)
+            {
+                pizzaStack[i].sprite = fullPizza;
+            }
+            else
+            {
+                pizzaStack[i].sprite = emptyPizza;
+            }
+
+            if (i < maxpizzas)
+            {
+                pizzaStack[i].enabled = true;
+            }
+            else
+            {
+                pizzaStack[i].enabled = false;
+            }
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (hasCollided)
+            return;
+
+        pizzas -= amount;
+
+        if (pizzas <= 0)
+        {
+            playerMovement.enabled = false;
+        }
+        hasCollided = true;
+    }
+}
