87ca432f23b73955df0bdbaa192394a803a7e9fc
Patrik Lundqvist <ph.lundqvist@gmail.com>
2024-05-19 23:17:12 +0200

    feat(Ball trap): trap now able to reset + added movement
    - Trap resets after 9 seconds
    - Ball trap can now be rotated by DM

--- Parcel Pandemonium/Assets/Scripts/Rotate120Degrees.cs
+++ Parcel Pandemonium/Assets/Scripts/Rotate120Degrees.cs
@@ -6,16 +6,21 @@ public class Rotate120Degrees : MonoBehaviour
 {
     public bool rotateClockwiseOrCounterClockwise = true;
     private bool startRotating = false;
+    private bool rotateBack = false;
+
     public void Rotate()
     {
-        startRotating = true;
+        if(!startRotating && !rotateBack){
+            startRotating = true;
+        }
     }
 
     // Rotates 120 slowly, stops after 120 degrees
     void Update()
     {
-        if (startRotating)
+        if (startRotating && !rotateBack)
         {
+            StartCoroutine(RotateBack());
             if (rotateClockwiseOrCounterClockwise)
             {
                 transform.Rotate(Vector3.up, 1);
@@ -31,8 +36,33 @@ public class Rotate120Degrees : MonoBehaviour
             else if (!rotateClockwiseOrCounterClockwise && transform.localRotation.eulerAngles.y <= 50)
             {
                 startRotating = false;
-            }
-            
+            }   
         }
+    
+        if (rotateBack)
+        {   
+            StartCoroutine(Stop());
+            if (rotateClockwiseOrCounterClockwise)
+            {
+                transform.Rotate(Vector3.up, -1);
+            }
+            else
+            {
+                transform.Rotate(Vector3.up, 1);
+            }
+        } 
+    }
+
+    IEnumerator Stop()
+    {
+        yield return new WaitForSeconds(2f);
+        rotateBack = false;
+    }
+
+    IEnumerator RotateBack()
+    {
+        yield return new WaitForSeconds(5);
+        startRotating = false;
+        rotateBack = true;
     }
 }
