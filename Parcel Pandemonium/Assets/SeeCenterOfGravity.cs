90d7c646c95f2a104c35aed8239d740d122bc6b8
Patrik Lundqvist <ph.lundqvist@gmail.com>
2024-05-26 23:30:37 +0200

    enhancement: lower center of gravity of driver player

---
+++ Parcel Pandemonium/Assets/SeeCenterOfGravity.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeCenterOfGravity : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Transform centerOfGravity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if (centerOfGravity)
+        {
+            rb.centerOfMass = centerOfGravity.localPosition;
+        }
+    }
+}
+
